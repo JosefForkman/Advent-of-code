3eb9f80aa7c56ce4cb23201a0d2bd1a5be784564
JosefForkman <strand.vatten@outlook.com>
2024-12-03 20:34:01 +0100

    Update challenge_2.js

--- 2024/day 3/challenge_2.js
+++ 2024/day 3/challenge_2.js
@@ -1,77 +1,30 @@
 import { readFileSync } from "fs";
 
-const file = readFileSync("input1.txt", { encoding: "utf8" });
-const fileArray = file
-    .split("\r\n")
-    .map((file) => file.split(" ").map((file) => Number(file)));
+let file = readFileSync("input1.txt", { encoding: "utf8" });
+file = `do()${file}`;
+const matches = file.match(/do\((.*?)don't\(\)/g) ?? [];
 
-const status = [];
-for (let i = 0; i < fileArray.length; i++) {
-    const levels = fileArray[i];
-    const first = levels.at(0);
-    const last = levels.at(-1);
+const formets = matches.map((matche) => {
+    const result = matche.match(/mul\(\d+,\d+\)/g);
 
-    const isIncreasing = first <= last;
+    return result;
+});
 
-    const row = [];
-    for (let j = 0; j < levels.length; j++) {
-        const beforeLevel = levels[j - 1];
-        const currentLevel = levels[j];
-        const afterLevel = levels[j + 1];
+const t = formets
+    .map((matche) => {
+        const results = [...matche.map((ma) => ma.match(/\((.*),(.*)\)/))].map(
+            (result) => [Number(result[1]), Number(result[2])],
+        );
+        // console.log(results);
 
-        if (isIncreasing) {
-            if (afterLevel) {
-                const value = afterLevel - currentLevel;
+        return results;
+    })
+    .flat();
+console.log(t.filter((f) => f.length != 2));
 
-                // const t = levels.filter((level) => currentLevel > afterLevel);
-                if (value < 1) {
-                    levels.splice(j, 1);
-                }
-            }
-        } else {
-            if (beforeLevel) {
-                const value = beforeLevel - currentLevel;
-
-                // const t = levels.filter((level) => currentLevel < beforeLevel);
-                // console.log(t);
-                
-                if (value < 1) {
-                    levels.splice(j, 1);
-                }
-            }
-        }
-    }
-    for (let j = 0; j < levels.length; j++) {
-        if (j != 0) {
-            const currentLevel = levels[j];
-            const beforeLevel = levels[j - 1];
-
-            if (isIncreasing) {
-                const value = currentLevel - beforeLevel;
-
-                row.push(value >= 1 && value <= 3);
-            } else {
-                const value = beforeLevel - currentLevel;
-                row.push(value >= 1 && value <= 3);
-            }
-        }
-    }
-    // console.log(row);
-
-    status.push(row);
-}
-
-
-
-console.log(status);
-
-const sulusions = [];
-status.forEach((level) => sulusions.push(level.every((lev) => lev)));
-// console.log(sulusions);
-
-const sulusion = sulusions.reduce(
+const sulusion = t.reduce(
     (previousValue, currentValue) =>
-        currentValue ? previousValue + 1 : previousValue,
+        previousValue += currentValue[0] * currentValue[1],
     0,
 );
 
