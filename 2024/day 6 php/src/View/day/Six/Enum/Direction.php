d915748a8d305ce8864bbd6c9e36ca45b83b4c50
JosefForkman <44498331+JosefForkman@users.noreply.github.com>
2025-07-27 08:34:51 +0100

    move class and enum to own folders

---
+++ 2024/day 6 php/src/View/day/Six/Enum/Direction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Adventofcode\Day6\View\day\Six\Enum;
+
+enum Direction: int
+{
+    case UP = 0;
+    case DOWN = 1;
+    case LEFT = 2;
+    case RIGHT = 3;
+}
---
+++ 2024/day 6 php/src/View/day/Six/Utils/Grid.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Adventofcode\Day6\View\day\Six\Utils;
+class Grid extends Cord
+{
+    public function __construct(
+        public int  $x,
+        public int  $y,
+        public bool $obstacle = false,
+    )
+    {
+        parent::__construct($this->x, $this->y);
+    }
+}
