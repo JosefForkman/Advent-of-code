e6193c9c8d64e8dacb8c80bf18bd708b68e87442
JosefForkman <44498331+JosefForkman@users.noreply.github.com>
2025-07-27 07:43:50 +0100

    add fontawesome

--- 2024/day 6 php/includes/header.php
+++ 2024/day 6 php/includes/header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a913f4ac89.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/src/css/style.css">
     <title>This is a test</title>
 </head>
